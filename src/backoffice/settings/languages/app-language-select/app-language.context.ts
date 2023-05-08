cbe8293112b8efa30ee8281dbc9fcb9f56020209
Mads Rasmussen <madsr@hey.com>
2023-05-08 17:31:15 +0200

    use context base

--- src/backoffice/settings/languages/app-language-select/app-language.context.ts
+++ src/backoffice/settings/languages/app-language-select/app-language.context.ts
@@ -1,11 +1,10 @@
 import { UmbLanguageRepository } from '../repository/language.repository';
 import { UmbObjectState, UmbObserverController } from '@umbraco-cms/backoffice/observable-api';
 import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller';
-import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
+import { UmbContextBase, UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 import { LanguageResponseModel } from '@umbraco-cms/backoffice/backend-api';
 
-export class UmbAppLanguageContext {
-	#host: UmbControllerHostElement;
+export class UmbAppLanguageContext extends UmbContextBase<UmbAppLanguageContext> {
 	#languageRepository: UmbLanguageRepository;
 
 	#languages: Array<LanguageResponseModel> = [];
@@ -14,8 +13,8 @@ export class UmbAppLanguageContext {
 	appLanguage = this.#appLanguage.asObservable();
 
 	constructor(host: UmbControllerHostElement) {
-		this.#host = host;
-		this.#languageRepository = new UmbLanguageRepository(this.#host);
+		super(host, UMB_APP_LANGUAGE_CONTEXT_TOKEN);
+		this.#languageRepository = new UmbLanguageRepository(host);
 		this.#observeLanguages();
 	}
 
@@ -27,7 +26,7 @@ export class UmbAppLanguageContext {
 	async #observeLanguages() {
 		const { asObservable } = await this.#languageRepository.requestLanguages();
 
-		new UmbObserverController(this.#host, asObservable(), (languages) => {
+		new UmbObserverController(this.host, asObservable(), (languages) => {
 			this.#languages = languages;
 
 			// If the app language is not set, set it to the default language
