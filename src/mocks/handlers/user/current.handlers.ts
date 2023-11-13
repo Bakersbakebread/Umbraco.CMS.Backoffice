c6bcc827363c38cf178fcee0438457ede6bdadc4
Mads Rasmussen <madsr@hey.com>
2023-11-13 12:47:22 +0100

    add mock handler to change current user password

--- src/mocks/handlers/user/current.handlers.ts
+++ src/mocks/handlers/user/current.handlers.ts
@@ -8,4 +8,15 @@ export const handlers = [
 		const loggedInUser = umbUsersData.getCurrentUser();
 		return res(ctx.status(200), ctx.json(loggedInUser));
 	}),
+
+	rest.post(umbracoPath(`${slug}/current/change-password`), async (req, res, ctx) => {
+		const data = await req.json();
+		if (!data) return;
+		if (!data.oldPassword) return;
+		if (!data.newPassword) return;
+
+		/* we don't have to update any mock data when a password is changed
+		so we just return a 200 */
+		return res(ctx.status(200));
+	}),
 ];
